53d25b55f881612ce1c130ed051f7ed65bc18a6b
lucapires10 <cartolaod4@gmail.com>
2026-05-27 19:23:13 -0300

    arrumei novamente

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var nome =document.querySelector('#nome_aluno').value;
 var nota =document.querySelector('#nota_aluno').value;
 console.log(nota);
 if (nota <=5){
-alert(' O (a)  ' + Nome + ' esta reprovado ' + nota);
+alert(' O (a) ' + Nome + ' esta reprovado '+ nota);
 
 }
 if(nota >5 && nota<7){
-   alert('O (a) aluno (a) ' +nome + ' esta de recuperacao ' + nota );
+   alert('O (a) aluno (a) ' + nome + ' esta de recuperacao '+ nota );
 }
 
  if (nota >=7){
