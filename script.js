67338b2dc525a8d7416ff1de559c16d023009b14
lucapires10 <cartolaod4@gmail.com>
2026-05-27 17:39:02 -0300

    exercicio de condiçoes

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function calcula(){
+//alert( 'nota do aluno' );
+var nome =document.querySelector('#nome_aluno').value;
+var nota =document.querySelector('#nota_aluno').value;
+console.log(nota);
+if (nota <=5){
+alert(' O' + nome +'esta reprovado'+ nota);
+
+}
+if(nota >5 && nota<7){
+   alert('Em' +nome + 'recuperacao'+ nota);
+}
+
+ if (nota >=7){
+alert('O (a) '  + nome + ' foi aprovado ' + nota);
+
+}
+
+}  
